ebd9603091519f5097403af6969860a568574d6d
Danil Bessonov <danbes1997@gmail.com>
2022-09-26 11:41:46 +0300

    there has been added the example of generation and subscribing of data thread by button click

--- app/src/main/java/com/example/jetbrains_idea_android_projects/RootFragment.kt
+++ app/src/main/java/com/example/jetbrains_idea_android_projects/RootFragment.kt
@@ -1,6 +1,7 @@
 package com.example.jetbrains_idea_android_projects
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
@@ -8,12 +9,37 @@ import com.example.jetbrains_idea_android_projects.databinding.FragmentRootBindi
 import io.reactivex.rxjava3.core.Observable
 
 class RootFragment : Fragment() {
+    private var _binding: FragmentRootBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ) = FragmentRootBinding.inflate(inflater, container, false).run {
-        Observable.just("Hello world!").subscribe { s -> textView.setText(s) }
+        _binding = this
+        button.setOnClickListener {
+            Observable.fromArray("Кошачья правда", "Игривые кошки", "Кот лета")
+                .subscribe { s -> Log.i("Rx", "onNext: $s") }
+        }
         root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = RootFragment().apply {
+            arguments = Bundle()
+        }
     }
 }
