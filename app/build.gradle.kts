ad848d3b070b91e3ba4f5ebdc8d7ec1c49685437
Danil Bessonov <danbes1997@gmail.com>
2022-09-26 08:50:20 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+@file:Suppress("UnstableApiUsage")
+
+import org.gradle.api.JavaVersion.VERSION_11
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.example.jetbrains_idea_android_projects"
+        minSdk = 21
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile(name = "proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = VERSION_11
+        targetCompatibility = VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = VERSION_11.toString()
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+    testImplementation("com.google.truth:truth:1.1.3")
+    androidTestImplementation("com.google.truth:truth:1.1.3")
+}
