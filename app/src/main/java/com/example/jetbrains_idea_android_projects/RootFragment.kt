0ae152d0a8fd3add29c7ffc3b7455726ed6a9fae
Danil Bessonov <danbes1997@gmail.com>
2022-09-26 11:09:34 +0300

    easy example of using rxKotlin has been added

--- app/src/main/java/com/example/jetbrains_idea_android_projects/RootFragment.kt
+++ app/src/main/java/com/example/jetbrains_idea_android_projects/RootFragment.kt
@@ -5,35 +5,15 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.jetbrains_idea_android_projects.databinding.FragmentRootBinding
+import io.reactivex.rxjava3.core.Observable
 
 class RootFragment : Fragment() {
-    private var _binding: FragmentRootBinding? = null
-    private val binding
-        get() = _binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ) = FragmentRootBinding.inflate(inflater, container, false).run {
-        _binding = this
+        Observable.just("Hello world!").subscribe { s -> textView.setText(s) }
         root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance() = RootFragment().apply {
-            arguments = Bundle()
-        }
     }
 }
